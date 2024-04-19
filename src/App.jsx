42029850c74d44c7e17f5fb59d34a5dac5065186
workdotnisha <workdotnisha@gmail.com>
2024-04-19 16:38:18 +0530

    Modifying components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import CurrentTime from "./components/CurrentTime";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
  
 
   return (
-    <>
-      <div>clock</div>
-    </>
+    <center>
+      <ClockHeading/>
+      <ClockSlogan/>
+      <CurrentTime/>
+    </center>
   )
 }
 
---
+++ src/components/ClockHeading.jsx
@@ -0,0 +1,5 @@
+function ClockHeading (){
+    return <h1 className="fw-bolder">Bharat Clock</h1>;
+}
+
+export default ClockHeading;
